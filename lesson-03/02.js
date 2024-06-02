2edb5cbb5589b5ffa653bd153268fc9b24e7385e
Itroman123 <143260109+Itroman123@users.noreply.github.com>
2024-06-02 13:42:41 +0300

    ...

---
+++ lesson-03/02.js
@@ -0,0 +1,8 @@
+​const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+const orange = words.indexOf("апельсин");
+
+if (orange !== -1){
+    console.log("Ура! нашел");
+}else{
+    console.log("Придется поискать в другом магазине…");
+}
